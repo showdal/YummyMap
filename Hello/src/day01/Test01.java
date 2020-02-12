8588976f7a082676a426210ace0ed43fe7387066
USER <USER@192.168.0.8>
2020-02-12 14:16:40 +0900

    1st commit

---
+++ Hello/src/day01/Test01.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Test01 {
+	public static void main(String[] args) {
+		String stringNumber1 = "abcd"; // camel 표기법
+		
+		String string_nember2 = "efgh"; // snake 표기법
+		
+		System.out.println("변수1 : " + stringNumber1);
+		System.out.println("변수2 : " + string_nember2);
+	}
+}
+
+class Test01_01{
+	public static void main(String[] args) {
+		
+	}
+
+}
